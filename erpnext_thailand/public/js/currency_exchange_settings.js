c106db80dec10f71b998f66d6ceb74e56d120907
newtratip <tharathip.chaweewongphan@gmail.com>
2025-03-26 18:26:45 +0700

    tab line and single quote to double quote

--- erpnext_thailand/public/js/currency_exchange_settings.js
+++ erpnext_thailand/public/js/currency_exchange_settings.js
@@ -1,64 +1,64 @@
-frappe.ui.form.on('Currency Exchange Settings', {
-    refresh(frm) {
-        if (frm.doc.service_provider === 'Bank of Thailand') {
-            frm.toggle_display('use_http', false);
-        }
-    },
+frappe.ui.form.on("Currency Exchange Settings", {
+	refresh(frm) {
+		if (frm.doc.service_provider === "Bank of Thailand") {
+			frm.toggle_display("use_http", false);
+		}
+	},
 
-    set_table_parameters: function (frm) {
-        // Clear existing rows
-        frm.clear_table('req_params');
+	set_table_parameters: function (frm) {
+		// Clear existing rows
+		frm.clear_table("req_params");
 
-        // Example data to add
-        const set_parameters = [
-            { key: 'from_currency', value: '{from_currency}' },
-            { key: 'to_currency', value: 'THB' },
-            { key: 'transaction_date', value: '{transaction_date}' }
-        ];
+		// Example data to add
+		const set_parameters = [
+			{ key: "from_currency", value: "{from_currency}" },
+			{ key: "to_currency", value: "THB" },
+			{ key: "transaction_date", value: "{transaction_date}" }
+		];
 
-        // Add new rows
-        set_parameters.forEach(data => {
-            const child = frm.add_child('req_params');
-            frappe.model.set_value(child.doctype, child.name, 'key', data.key);
-            frappe.model.set_value(child.doctype, child.name, 'value', data.value);
-        });
-        // Refresh the child table to show changes
-        frm.refresh_field('req_params');
-    },
-    set_table_result_key: function (frm) {
-        // Clear existing rows
-        frm.clear_table('result_key');
+		// Add new rows
+		set_parameters.forEach(data => {
+			const child = frm.add_child("req_params");
+			frappe.model.set_value(child.doctype, child.name, "key", data.key);
+			frappe.model.set_value(child.doctype, child.name, "value", data.value);
+		});
 
-        // Example data to add
-        const set_result_key = [
-            { key: 'message' },
-            { key: 'rates' },
-            { key: 'THB' }
-        ];
+		// Refresh the child table to show changes
+		frm.refresh_field("req_params");
+	},
 
-        // Add new rows
-        set_result_key.forEach(data => {
-            const child = frm.add_child('result_key');
-            frappe.model.set_value(child.doctype, child.name, 'key', data.key);
-        });
+	set_table_result_key: function (frm) {
+		// Clear existing rows
+		frm.clear_table("result_key");
 
-        // Refresh the child table to show changes
-        frm.refresh_field('result_key');
-    },
+		// Example data to add
+		const set_result_key = [
+			{ key: "message" },
+			{ key: "rates" },
+			{ key: "THB" }
+		];
 
-    service_provider: function (frm) {
-        if (frm.doc.service_provider === 'Bank of Thailand') {
-            const base_url = window.location.origin
-            frm.set_value("api_endpoint", `${base_url}/api/v2/method/erpnext_thailand.custom.currency_exchange_bot_api.get_api_currency_exchange`);
-            frm.set_value("url", null)
-            frm.toggle_display('use_http', false);
+		// Add new rows
+		set_result_key.forEach(data => {
+			const child = frm.add_child("result_key");
+			frappe.model.set_value(child.doctype, child.name, "key", data.key);
+		});
 
-            frm.events.set_table_parameters(frm);
-            frm.events.set_table_result_key(frm);
+		// Refresh the child table to show changes
+		frm.refresh_field("result_key");
+	},
 
+	service_provider: function (frm) {
+		if (frm.doc.service_provider === "Bank of Thailand") {
+			const base_url = window.location.origin
+			frm.set_value("api_endpoint", `${base_url}/api/v2/method/erpnext_thailand.custom.currency_exchange_bot_api.get_api_currency_exchange`);
+			frm.set_value("url", null)
+			frm.toggle_display("use_http", false);
 
+			frm.events.set_table_parameters(frm);
+			frm.events.set_table_result_key(frm);
         } else {
-            frm.toggle_display('use_http', true);
+            frm.toggle_display("use_http", true);
         }
     }
 });
