bc6a251a6f862f2f334d65784070f4ec9bca8fe9
theerayut <theerayutattajak@gmail.com>
2025-05-31 00:58:16 +0700

    Get address from customer/supplier

--- erpnext_thailand/public/js/address.js
+++ erpnext_thailand/public/js/address.js
@@ -85,6 +85,14 @@ frappe.ui.form.on("Address", {
 			);
 		}, __("Get Address"));
 
+		// Add button to get address by customer or supplier
+		frm.add_custom_button(
+			__("By Customer / Supplier"),
+			() => {
+				open_type_selection_dialog(frm);
+			},
+			__("Get Address")
+		);
 	},
 
 	get_address_by_tax_id: function (frm, filters) {
@@ -108,3 +116,99 @@ frappe.ui.form.on("Address", {
 		});
 	},
 });
+
+function open_type_selection_dialog(frm) {
+	const dialog = new frappe.ui.Dialog({
+		title: __("Enter Details"),
+		fields: [
+			{
+				fieldtype: "Select",
+				label: __("Select Type"),
+				fieldname: "type",
+				options: ["Customer", "Supplier"],
+				reqd: 1,
+			},
+			{
+				fieldtype: "Link",
+				label: __("Select Customer/Supplier"),
+				fieldname: "customer_supplier",
+				options: "", // updated dynamically
+				reqd: 1,
+			},
+		],
+		primary_action_label: __("Get Location"),
+		primary_action(values) {
+			fetch_partner_locations(values.customer_supplier, values.type, frm);
+			dialog.hide();
+		},
+	});
+
+	// Dynamically change Link options based on type
+	dialog.fields_dict.type.$input.on("change", () => {
+		const selected_type = dialog.get_value("type");
+		dialog.set_df_property("customer_supplier", "options", selected_type);
+	});
+
+	dialog.show();
+}
+
+function fetch_partner_locations(customer_supplier, type, frm) {
+	frappe.call({
+		method: "erpnext_thailand.utils.get_partner_address",
+		args: { customer_supplier, type },
+		callback({ message }) {
+			if (!message || message.length === 0) {
+				frappe.msgprint(__("No location found for the provided customer/supplier."));
+				return;
+			}
+
+			if (message.length === 1) {
+				apply_location_to_form(message[0], frm);
+			} else {
+				open_location_selection_dialog(message, frm);
+			}
+		},
+	});
+}
+
+function open_location_selection_dialog(locations, frm) {
+	const options = locations.map(
+		(loc) =>
+			`${loc.address_line1}, ${loc.address_line2}, ${loc.city}, ${loc.county}, ${loc.state}, ${loc.country}, ${loc.pincode}`
+	);
+
+	const dialog = new frappe.ui.Dialog({
+		title: __("Select Data"),
+		fields: [
+			{
+				fieldtype: "Select",
+				label: __("Select Option"),
+				fieldname: "selected_option",
+				options: options.join("\n"),
+				reqd: 1,
+			},
+		],
+		primary_action_label: __("Submit"),
+		primary_action(data) {
+			dialog.hide();
+			const selected = locations.find(
+				(loc) =>
+					`${loc.address_line1}, ${loc.address_line2}, ${loc.city}, ${loc.county}, ${loc.state}, ${loc.country}, ${loc.pincode}` ===
+					data.selected_option
+			);
+			if (selected) apply_location_to_form(selected, frm);
+		},
+	});
+
+	dialog.show();
+}
+
+function apply_location_to_form(location, frm) {
+	frm.set_value("address_line1", location.address_line1);
+	frm.set_value("address_line2", location.address_line2);
+	frm.set_value("city", location.city);
+	frm.set_value("county", location.county);
+	frm.set_value("state", location.state);
+	frm.set_value("country", location.country);
+	frm.set_value("pincode", location.pincode);
+}
