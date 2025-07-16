40183bbec270d180f91bed6ab0774255675a994b
Kitti U. @ Ecosoft <kittiu@gmail.com>
2025-07-16 22:40:54 +0700

    [FIX] Returning Invoice must not use both negative rate and qty when create Tax Invoice (#45)

--- erpnext_thailand/custom/gl_entry.py
+++ erpnext_thailand/custom/gl_entry.py
@@ -1,19 +1,18 @@
 import frappe
 from frappe.model.naming import set_name_from_naming_options
+from frappe.utils import now
 
 
 def rename_temporarily_named_docs(doctype):
 	"""Rename temporarily named docs using autoname options"""
-	docs_to_rename = frappe.get_all(
-		doctype, {"to_rename": "1"}, order_by="creation", limit=50000
-	)
+	docs_to_rename = frappe.get_all(doctype, {"to_rename": "1"}, order_by="creation", limit=50000)
 	for doc in docs_to_rename:
 		oldname = doc.name
 		set_name_from_naming_options(frappe.get_meta(doctype).autoname, doc)
 		newname = doc.name
 		frappe.db.sql(
-			f"UPDATE `tab{doctype}` SET name = %s, to_rename = 0 where name = %s",
-			(newname, oldname),
+			f"UPDATE `tab{doctype}` SET name = %s, to_rename = 0, modified = %s where name = %s",
+			(newname, now(), oldname),
 			auto_commit=True,
 		)
 		# Monkey patch
