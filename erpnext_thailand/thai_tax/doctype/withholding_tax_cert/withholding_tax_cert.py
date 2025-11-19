1c77fd24b7996e851cbaf3bd5faac7f831edd6ac
Saralrat <saralrats@ecosoft.co.th>
2025-11-19 15:23:57 +0700

    [FIX]: round tax_amount to 2 decimal places in Withholding Tax with JS

--- erpnext_thailand/thai_tax/doctype/withholding_tax_cert/withholding_tax_cert.py
+++ erpnext_thailand/thai_tax/doctype/withholding_tax_cert/withholding_tax_cert.py
@@ -3,10 +3,7 @@
 
 # import frappe
 from frappe.model.document import Document
-from frappe.utils import flt
 
 
 class WithholdingTaxCert(Document):
-	def validate(self):
-		for item in self.withholding_tax_items:
-			item.tax_amount = flt((item.tax_base or 0) * (item.tax_rate or 0) / 100, 2)
+	pass
